647a5241e9e83bf373b006bb3421eec43f28f033
AlessioSanna <noticebox.job@outlook.com>
2025-09-14 18:21:55 +0200

    coding navbar template

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand d-flex align-items-center">
-          <img id="logoPage" src="/images/pizza-logo-E6DE845BD3-seeklogo.com.png" alt="Logo" width="48" height="auto" />
+          <img id="logoPage" src="/images/pizza-logo-E6DE845BD3-seeklogo.com.png" alt="Logo" width="64" height="auto" />
           <span className="ms-2">THE PIZZA HOUSE</span>
         </Link>
 
@@ -15,12 +15,9 @@ export default function Navbar() {
 
         <div className="collapse navbar-collapse" id="nav">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <NavLink to="/menu" className="nav-link">Menu</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/ordina" className="nav-link">Ordina</NavLink>
-            </li>
+            <li className="nav-item"><NavLink to="/menu" className="nav-link">Menu</NavLink></li>
+            <li className="nav-item"><NavLink to="/ordina" className="nav-link">Ordina</NavLink></li>
+            <li className="nav-item"><a className="nav-link" href="#therealfooter">Contatti</a></li>
           </ul>
         </div>
       </div>
