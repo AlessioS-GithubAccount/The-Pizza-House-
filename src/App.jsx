a8f41a223f33f182a82352d0925f6c588daf476f
AlessioSanna <noticebox.job@outlook.com>
2025-09-14 18:21:04 +0200

    modify padding-top for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
-
-// pagine
 import Home from './pages/Home.jsx'
 import Menu from './pages/Menu.jsx'
 import Order from './pages/Order.jsx'
-
-// componenti layout
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 
@@ -13,15 +9,13 @@ export default function App() {
   return (
     <>
       <Navbar />
-
-      <main style={{ paddingTop: 80 }}>
+      <main style={{ paddingTop: 30 }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/ordina" element={<Order />} />
         </Routes>
       </main>
-
       <Footer />
     </>
   )
