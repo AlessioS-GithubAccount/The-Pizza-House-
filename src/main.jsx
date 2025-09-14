89d78f8dcb7c85c3c2a2133df5437a7e5c686f20
AlessioSanna <noticebox.job@outlook.com>
2025-09-14 22:19:59 +0200

    import bootstrap bundle

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 // Import UI: Bootstrap e CSS globale
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
