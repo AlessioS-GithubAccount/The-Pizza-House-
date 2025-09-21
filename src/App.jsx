7fdb0fc6706c7412764ba0feb9e6e3d42506b3d5
AlessioSanna <noticebox.job@outlook.com>
2025-09-21 14:25:24 +0200

    add route cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+// src/App.jsx
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Menu from './pages/Menu.jsx'
 import Order from './pages/Order.jsx'
@@ -8,18 +9,23 @@ import Register from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
 import ScrollToTop from './ScrollToTop.jsx'
 
+// 👇 Cart sta in components
+import Cart from './components/Cart.jsx'
+
 export default function App() {
   return (
     <>
       <Navbar />
-      <main style={{ paddingTop: 30 }}>
-        <ScrollToTop />  
-        <Routes>          
+      <main style={{ paddingTop: 72 }}>
+        <ScrollToTop />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />     
           <Route path="/ordina" element={<Order />} />
-          <Route path="/register" element={<Register />} /> 
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       <Footer />
